19f3bca35a86351c48e843b0933515b840377673
Daniel Konovalenko <ddkono@gmail.com>
2017-10-05 22:21:35 +0300

    Add Dish model to the admin site

--- junto_api/admin.py
+++ junto_api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Dish
 # Register your models here.
 
 
@@ -8,10 +8,27 @@ class CategoryInline(admin.TabularInline):
     verbose_name = 'Subcategory'
     verbose_name_plural = 'Subcategories'
     
-    
+
+class DishInline(admin.TabularInline):
+    model = Category.dishes.through
+    verbose_name = 'Dish'
+    verbose_name_plural = 'Dishes'
+
+
+class CategoryDishInline(admin.TabularInline):
+    model = Dish.categories.through
+    verbose_name = 'Category'
+    verbose_name_plural = 'Categories'
+
+
 class CategoryAdmin(admin.ModelAdmin):
     inlines = [
         CategoryInline,
+        DishInline
     ]
+    
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Dish, DishAdmin)
