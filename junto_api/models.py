ccf5b859f58716402215b7a0e8e8d5cacbe26563
Daniel Konovalenko <ddkono@gmail.com>
2017-10-05 22:21:12 +0300

    Add Dish model w/ tests

--- junto_api/models.py
+++ junto_api/models.py
@@ -2,15 +2,33 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Название")
+    name = models.CharField(max_length=100, verbose_name='Название')
     supercategory = models.ForeignKey('self',
                                       on_delete=models.SET_NULL,
                                       null=True,
+                                      blank=True,
                                       related_name='subcategories',
                                       related_query_name='subcategory')
 
     class Meta:
-        verbose_name_plural = "categories"
+        verbose_name_plural = 'categories'
     
     def __str__(self):
         return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    price = models.DecimalField(max_digits=8,
+                                decimal_places=2,
+                                verbose_name='Стоимость, ')
+    categories = models.ManyToManyField(Category,
+                                        related_name='dishes',
+                                        related_query_name='dish',
+                                        verbose_name='Категории')
+    
+    class Meta:
+        verbose_name_plural = 'Dishes'
+
+    def __str__(self):
+        return f'{self.name} ({self.price} ₽)'
--- junto_api/tests/test_models.py
+++ junto_api/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from junto_api.models import Category
+from junto_api.models import Category, Dish
 
 # Create your tests here.
 class CategoryTestCase(TestCase):
@@ -8,9 +8,13 @@ class CategoryTestCase(TestCase):
         self.burgers = Category.objects.create(name='Бургеры')
         self.beef_burgers = Category.objects.create(name='Бургеры из говядины')
         
+        self.fastfood.save()
+        self.burgers.save()
+        self.beef_burgers.save()
+        
         self.fastfood.subcategories.add(self.burgers)
         self.burgers.subcategories.add(self.beef_burgers)
-    
+        
     def test_subcategories(self):
         self.assertTrue(self.burgers in self.fastfood.subcategories.all())
         self.assertTrue(self.beef_burgers in self.burgers.subcategories.all())
@@ -18,3 +22,17 @@ class CategoryTestCase(TestCase):
     def test_supercategories(self):
         self.assertTrue(self.burgers.supercategory is self.fastfood)
         self.assertTrue(self.beef_burgers.supercategory is self.burgers)
+
+    def test_dish_relation(self):
+        texas_burger = Dish(name='Техасский бургер', price=250)
+        fries = Dish(name='Картофель фри', price=50)
+        
+        texas_burger.save()
+        fries.save()
+        
+        self.beef_burgers.dishes.add(texas_burger)
+        self.fastfood.dishes.add(fries)
+        
+        self.assertTrue(texas_burger in self.beef_burgers.dishes.all())
+        self.assertTrue(fries in self.fastfood.dishes.all())
+        
