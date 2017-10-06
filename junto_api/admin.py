7168d55fc373871c2115fc611fd3fb0dbc089548
Daniel Konovalenko <ddkono@gmail.com>
2017-10-06 16:54:48 +0300

    Add Order model to admin site

--- junto_api/admin.py
+++ junto_api/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
-from .models import Category, Dish, Restaurant
-# Register your models here.
+from .models import Category, Dish, Restaurant, Order, DishOrder
 
 
 class CategoryInline(admin.TabularInline):
     model = Category
     verbose_name = 'Subcategory'
     verbose_name_plural = 'Subcategories'
-    
+
 
 class DishInline(admin.TabularInline):
     model = Category.dishes.through
@@ -15,21 +14,29 @@ class DishInline(admin.TabularInline):
     verbose_name_plural = 'Dishes'
 
 
-class CategoryDishInline(admin.TabularInline):
-    model = Dish.categories.through
-    verbose_name = 'Category'
-    verbose_name_plural = 'Categories'
+class DishOrderInline(DishInline):
+    model = DishOrder
+    extra = 1
 
 
 class CategoryAdmin(admin.ModelAdmin):
     inlines = [
         CategoryInline,
-        DishInline
     ]
-    
+
+
 class DishAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price')
+    list_display = ['name', 'price']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['created_at', 'restaurant', 'operator', 'status', 'total']
+    inlines = [
+        DishOrderInline,
+    ]
+
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Dish, DishAdmin)
 admin.site.register(Restaurant)
+admin.site.register(Order, OrderAdmin)
