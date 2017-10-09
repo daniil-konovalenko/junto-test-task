463275b6d3c55d53e419a07e8c7698478369d9bf
Daniel Konovalenko <ddkono@gmail.com>
2017-10-09 17:27:02 +0300

    Add stub for token-based authentication view decorator

--- junto_api/auth.py
+++ junto_api/auth.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 import jwt
 import datetime
+from django.http import HttpRequest
+from django.http import HttpResponse
 
 
 def generate_access_token(user: User,
@@ -16,3 +18,16 @@ def generate_access_token(user: User,
             algorithm='HS256').decode()
         
         return token
+
+
+def token_required(function):
+    def wrap(request: HttpRequest, *args, **kwargs):
+        auth_header =  request.META.get('HTTP_AUTHORIZATION')
+        print(request.META)
+        print(auth_header)
+        if auth_header is not None:
+            
+            return function(request, *args, **kwargs)
+        else:
+            return HttpResponse('Unauthorized', status=401)
+    return wrap
