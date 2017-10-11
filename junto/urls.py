4761c1ed65442917efa0e9c5821212aa7b4bffa1
Daniel Konovalenko <ddkono@gmail.com>
2017-10-12 00:29:46 +0300

    Redirect empty path to the admin site

--- junto/urls.py
+++ junto/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     url(r'^api/', include('junto_api.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', RedirectView.as_view(url='/admin'))
 ]
